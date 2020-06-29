74a3c98bbefd8f369687a40ed3aac1db95a58f4a
tranhoandz98 <tranhoan.dz98@gmail.com>
2020-06-30 02:41:56 +0700

    done db

--- database/migrations/2020_06_28_175928_create_attrs_table.php
+++ database/migrations/2020_06_28_175928_create_attrs_table.php
@@ -4,33 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAttrsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('attrs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->unique();
-            $table->string('slug');
-            $table->string('value');
-            $table->unsignedInteger('id_pro_detail');
-            $table->tinyInteger('status')->default(1)->comment('1 la Hien, 0 la An');
-            $table->timestamps();
-        });
-    }
+class CreateAttrsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('attrs', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name')->unique();
+			$table->string('slug');
+			$table->string('value');
+			// $table->unsignedInteger('id_pro_detail');
+			$table->tinyInteger('status')->default(1)->comment('1 la Hien, 0 la An');
+			$table->timestamps();
+			// $table->foreign('id_pro_detail')->references('id')->on('product_details');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('attrs');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('attrs');
+	}
 }
